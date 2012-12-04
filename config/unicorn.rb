3df6eb9bb68e9702b3ef49e32252a325af07e8f8
Kun <cleo.k.yang@gmail.com>
2012-12-04 14:13:08 -0500

    add unicorn config for capistrano

---
+++ config/unicorn.rb
@@ -0,0 +1,19 @@
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+# >= num of cpus/cores
+worker_processes 4
+
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+working_directory "/home/deployer/applications/partsBuilder/current"
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/partsBuilder.sock", :backlog => 64
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+# feel free to point this anywhere accessible on the filesystem
+shared_path = "/home/deployer/applications/partsBuilder/shared"
+pid "#{shared_path}/pids/unicorn.pid"
