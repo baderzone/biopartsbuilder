1be27822b66b9a330b965d496ba8470f492053fb
Kun Yang <cleo.k.yang@gmail.com>
2015-01-22 13:59:38 -0500

    upgrade google authentication to oauth2

--- Gemfile
+++ Gemfile
@@ -6,8 +6,7 @@ gem 'rails', '3.2.13'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
-gem 'omniauth'
-gem 'omniauth-openid'
+gem "omniauth-google-oauth2"
 gem 'cancan'
 
 # Gems used only for assets and not required
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :openid, :name => 'myopenid', :identifier => 'myopenid.com'
-  provider :openid, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-end
+Rails.application.config.middleware.use OmniAuth::Builder do  
+  provider :google_oauth2, "485409189971-5qrhq51426khm69v0rr3uc7vp4b8ej5l.apps.googleusercontent.com", "wJLPNQV4z7iYktihuB989KvV"
+end 
